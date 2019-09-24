be6631ff18abdd1fa17997c36509e966e11bd2dd
javier12casta <jcastaeda10@gmail.com>
2019-09-24 11:14:53 -0500

    estructura

---
+++ src/app/interfaces/puntoentrega.ts
@@ -0,0 +1,13 @@
+export interface Puntoentrega {
+    idPuntoEntrega?: number,
+    NombrePE: string,
+    CodigoInternoPE: string,
+    Direccion: string,
+    Responsable: string,
+    Suplente: string,
+    Estado: number,
+    Telefono: string,
+    CodigoExternoPE: number,
+    idBarriosVeredas: number,
+    idCentrosZonales: number,
+}
