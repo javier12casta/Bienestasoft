4e664c7f40bb43cabe460305324d7875cddd59c3
majimenezso <alejo-taizon@hotmail.com>
2019-10-01 22:23:24 -0500

    Registro Usuarios
    Registro Usuarios

--- src/app/page/datos-generales-usuario/datos-generales-usuario.component.ts
+++ src/app/page/datos-generales-usuario/datos-generales-usuario.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit , Input } from '@angular/core';
 import { Usuarios } from 'src/app/interfaces/usuarios';
-
+import { ServicioService } from 'src/app/servicio.service';
 @Component({
   selector: 'app-datos-generales-usuario',
   templateUrl: './datos-generales-usuario.component.html',
@@ -71,29 +71,21 @@ import { Usuarios } from 'src/app/interfaces/usuarios';
 
 		</head>
 		<body>
-                <div class="padre">
-				<div id="header" class="hijo_uno">
-						<ul class="nav">
-													
-							<li><a routerLink="/datosgeneralesusuario">Datos Generales</a></li>
-							<li><a routerLink="//usuariosroles">Usuario y Asignación de Roles</a></li>
-							<li><a routerLink="/datoscontactousuario">Datos de contacto</a></li>
-					
-						</ul>
-					</div>
-				</div>
+               
 		<h1>Crear Usuario</h1>
 
-		<h1>Datos Generales Usuario</h1>
+		
 		<div class="container"  class="padre">
 		 <div class="card card-container" >
 		
 		<form class="form-signin padding" >
 		<h2></h2>
+		<h1>Datos Generales Usuario</h1>
+		<h2></h2>
 		<span class="btn btn-success" >Estado del dato Maestro</span>
 		<h2></h2>
 		
-		<select name="estadomaestro" [(ngModel)]="maestro" class="select-css">
+		<select name="estadomaestro" [(ngModel)]="x.Estado" class="select-css">
   <option [value]="item" *ngFor="let item of listamaestro">{{item}} </option>
   </select>
  
@@ -102,7 +94,7 @@ import { Usuarios } from 'src/app/interfaces/usuarios';
 		<span class="btn btn-success">Tipo Documento</span>
 		<h2></h2>
 		
-    <select name="tipodocumento" [(ngModel)]="tipdoc" class="select-css">
+    <select name="tipodocumento" class="select-css">
     <option [value]="item" *ngFor="let item of listadoc">{{item}} </option>
     </select>
 		
@@ -117,24 +109,96 @@ import { Usuarios } from 'src/app/interfaces/usuarios';
     <h2></h2>
 		<span class="btn btn-success">Nombres</span>
 		<h2></h2>
-		<input (keyup)="Nombres($event)" id="inputNombres" placeholder="Nombres" type="text">
+		<input type="text" class="form-control" [(ngModel)]="x.Nombres" name="nombre"
+		placeholder="Nombres" class="form-control" id="nom">
+    
   
 		<h2></h2>
 		<span class="btn btn-success">Apellidos</span>
 		<h2></h2>
-    <input (keyup)="Apellidos($event)" id="inputApllidos" placeholder="Apellidos" type="text">
+		<input type="text" class="form-control" [(ngModel)]="x.Apellidos" name="apellidos"
+		placeholder="Apellidos" class="form-control" id="app">
     <h2></h2>
   
 		<span class="btn btn-success">Fecha de Ingreso</span>
 		<h2></h2>
-		<input [(ngModel)]="today"  id="fechaingreso" min="2018-01-01" max="2050-12-31" type="date">
+		<input type="date" class="form-control" [(ngModel)]="x.FechaIngreso" name="fechai" placeholder="Fecha" min="2018-01-01" max="2050-12-31" class="form-control">
+	
          <h2></h2>
-         {{today}}
+		 {{today}}
+		 <h1>Usuario y Asignación de Roles</h1>
+		 <span class="btn btn-success" >Usuario en el sistema</span>
+  <h2></h2>
+  <input type="text" class="form-control" [(ngModel)]="x.NombreUsuarioSistema" name="Usuario"
+		placeholder="Usuario Sistema" class="form-control" id="usu">
 
-		</form><!-- /form -->
-		</div><!-- /card-container -->
-		</div><!-- /card-container -->
+  <h2></h2>
+  <span class="btn btn-success">Tipo Documento</span>
+  <h2></h2>
+  
+  <select name="tipos" [(ngModel)]="tipousuario" class="select-css">
+  <option [value]="item" *ngFor="let item of listat">{{item}} </option>
+  </select>
+  
+  
+             <h2></h2>
+             <span class="btn btn-success">Rol</span>
+             <h2></h2>
+      
+             <select name="rols" [(ngModel)]="tiporol" class="select-css">
+             <option [value]="item" *ngFor="let item of listar">{{item}} </option>
+             </select>
+		  <h2></h2>
+		  
+		  <h1>Datos de contacto</h1>
+		  <span class="btn btn-success" >Direccion</span>
+		  <h2></h2>
+		  
+		  
+		  <input type="text" class="form-control" [(ngModel)]="x.Direccion" name="Nombredireccion"
+					  placeholder="Direccion" class="form-control">
+		  
+		  
+		  <h2></h2>
+		  <span class="btn btn-success">Telefono Fijo 1</span>
+		  <h2></h2>
+		  
+		  <input type="number" class="form-control" [(ngModel)]="x.TelefonoFijo" name="telefonof1"
+					  placeholder="Telefono fijo 1" class="form-control">
+		  
+		  <h2></h2>
+		  <span class="btn btn-success">Telefono Fijo 2</span>
+		  <h2></h2>
+		  <input type="number" class="form-control" [(ngModel)]="x.TelefonoFijo2" name="telefonof2"
+					  placeholder="Telefono fijo 2" class="form-control">
+		  <h2></h2>
+		  <span class="btn btn-success">Telefono Movil 1</span>
+		  <h2></h2>
+		  <input type="number" class="form-control" [(ngModel)]="x.TelefonoMovil" name="telefonom1"
+					  placeholder="Telefono movil 1" class="form-control">
+		  <h2></h2>
+		  <span class="btn btn-success">Telefono movil 2</span>
+		  <h2></h2>
+		  <input type="number" class="form-control" [(ngModel)]="x.TelefonoMovil2" name="telefonom2"
+					  placeholder="Telefono movil 2" class="form-control">
+		  <h2></h2>
+		  <span class="btn btn-success">Email</span>
+		  <h2></h2>
+		  <input type="text" class="form-control" [(ngModel)]="x.Email" name="email"
+		  placeholder="Email" class="form-control">
+		  
+			   <h2></h2>
+			   
+				  
+			   <button (click)="onClickMe()" class="btn btn-success">Aceptar</button>
+				   {{clickMessage}}
+			   <h2></h2>
+		
+  </form><!-- /form -->
+  </div><!-- /card-container -->
+  </div><!-- /card-container -->
 
+  
 	</body>
 </html>
 
@@ -145,6 +209,8 @@ export class DatosGeneralesUsuarioComponent implements OnInit {
 
   listamaestro:string[]=["0","1"];
   listadoc:string[]=["CC","CE","PA"];
+  listat:string[]=["PE","UDS","ADMINISTRADOR"];
+  listar:string[]=["ROL 1","ROL 2","ROL 3","ROL 4","ROL 5","ROL 6","ROL 7","ROL 8","ROL 9","ROL 10"];
 
 
   x : Usuarios = {
@@ -153,8 +219,8 @@ export class DatosGeneralesUsuarioComponent implements OnInit {
     Apellidos : '',
     Estado : 0,
     NumeroDocumento : 0,
-    FechaIngreso : 1000,
-    NombreUsuarioSistema : 'hj65',
+    FechaIngreso : 0,
+    NombreUsuarioSistema : '',
     Direccion : '',
     TelefonoFijo :  0,
     TelefonoFijo2 : 0,
@@ -163,19 +229,37 @@ export class DatosGeneralesUsuarioComponent implements OnInit {
     Email : '',
 
   };
+
+  clickMessage = '';
+  valor1='';
+ 
   
-  @Input() N  = this.x.NumeroDocumento;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
-
-  obtenerdatos(){
-
-  this.x.NumeroDocumento;
-
-  }
+  onClickMe() {
+    
+    
+	this.valor1;
+	this.clickMessage = 'Se ha creado el usuario con acceso al sistema <número de identificación> en el nivel <primer nivel o Segundo nivel o Tercer nivel>.';
+	
+ 
+	this.Service.postUsuarios(this.x).subscribe(res => {
+	 console.log(this.x);
+   },
+	 err => {
+	   console.log(err);
+	 });
+	
+ }
+ 
+   constructor(private Service: ServicioService) { }
+ 
+   ngOnInit() {
+ 
+	
+ 
+   }
+ 
 
 
 }
