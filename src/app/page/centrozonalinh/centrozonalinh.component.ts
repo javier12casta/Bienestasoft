88bee1c64cb9fc374495c49c8c5da87631a8e580
javier alejandro castañeda celis <jcastaeda10@gmail.com>
2019-10-25 09:52:01 -0500

    validacion inhabilitar

--- src/app/page/centrozonalinh/centrozonalinh.component.ts
+++ src/app/page/centrozonalinh/centrozonalinh.component.ts
@@ -6,6 +6,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Municipio } from '../../interfaces/municipio';
 import { Regional } from '../../interfaces/regional';
 import Swal from 'sweetalert2';
+import { Puntoentrega } from 'src/app/interfaces/puntoentrega';
 
 @Component({
   selector: 'app-centrozonalinh',
@@ -15,14 +16,20 @@ import Swal from 'sweetalert2';
 export class CentrozonalinhComponent implements OnInit {
   public regional: Regional[] = [];
   municipios: Municipio[] = [];
-  public options: Select2Options;
-
-  constructor(
-    private activeRoute: ActivatedRoute,
-    private Service: ServicioService,
-  ) {
-  } 
-
+  puntos: Puntoentrega[] = [];
+  punto: Puntoentrega = {
+    idPuntoEntrega: 0,
+    NombrePE: '',
+    CodigoInternoPE: '',
+    Direccion: '',
+    Responsable: '',
+    Estado: 0,
+    Telefono: '',
+    CodigoExternoPE: '',
+    idCentrosZonales: 0,
+    BarrioPE: '',
+    Comuna: '',
+  };
   centros: Centrozonal = {
     idCentrosZonales: 0,
     NombreCentroZonal: '',
@@ -33,6 +40,15 @@ export class CentrozonalinhComponent implements OnInit {
     Estado: 1,
     idRegional: 2,
   };
+
+  public options: Select2Options;
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private Service: ServicioService,
+  ) {
+  }
+
 
   ngOnInit() {
 
@@ -48,6 +64,15 @@ export class CentrozonalinhComponent implements OnInit {
         }
         );
     }
+
+    this.Service.getPunto().subscribe(
+      res => {
+        this.punto = Object(res);
+        this.puntos = Object(res);
+      }, err => {
+        console.log(err);
+      }
+    );
     // Traer Muinicipios ---------------------------------------
     this.Service.getMunicipio()
       .subscribe(res => {
@@ -73,6 +98,25 @@ export class CentrozonalinhComponent implements OnInit {
 
   // Actualizar Datos---------------------------------------------
   updateDatos() {
+    const params = this.activeRoute.snapshot.params;
+    var numero = params.id;
+    for (let numeros of this.puntos) {
+      console.log(numeros.idCentrosZonales, numeros.Estado);
+      console.log(numero);
+      if (numero == numeros.idCentrosZonales && numeros.Estado == 1) {
+        console.log('Entro al if');
+        this.showMenssage3();
+      } else if(numero == numeros.idCentrosZonales && numeros.Estado == 0) {
+        this.updateDatos2();
+        console.log('Inhabilitado');
+      }
+    }
+
+  }
+
+
+  updateDatos2() {
+    // metodo update inhabilitar
     this.Service.putCentro(this.centros.idCentrosZonales, this.centros)
       .subscribe(
         res => {
@@ -84,22 +128,29 @@ export class CentrozonalinhComponent implements OnInit {
         }
       );
   }
-
-    //mensajes de creacion
-    showMenssage(){
-      Swal.fire({
-        title: 'Inhabilitado!',
-        text: 'Centro Zonal Inhabilitado',
-        type: 'success',
-        confirmButtonText: 'Entendido'
-      });
-    }
+  //mensajes de creacion
+  showMenssage() {
+    Swal.fire({
+      title: 'Inhabilitado!',
+      text: 'Centro Zonal Inhabilitado',
+      type: 'success',
+      confirmButtonText: 'Entendido'
+    });
+  }
   //Mensaje de error
-  
-  showMenssage2(){
+
+  showMenssage2() {
     Swal.fire({
       title: 'Error!',
       text: 'Error al inhabilitar el centro zonal',
+      type: 'error',
+      confirmButtonText: 'Entendido'
+    });
+  }
+  showMenssage3() {
+    Swal.fire({
+      title: 'Error!',
+      text: 'No es posible inhabilitar el centro zonal',
       type: 'error',
       confirmButtonText: 'Entendido'
     });
