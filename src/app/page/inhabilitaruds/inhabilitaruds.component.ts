c39c062aa92d9a392b4750cce18c1e9d2b034cfa
javier alejandro castañeda celis <jcastaeda10@gmail.com>
2019-10-26 15:52:39 -0500

    validaciones estructura organizacional

--- src/app/page/inhabilitaruds/inhabilitaruds.component.ts
+++ src/app/page/inhabilitaruds/inhabilitaruds.component.ts
@@ -4,6 +4,8 @@ import { ServicioService } from '../../servicio.service';
 import { ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
+import { Puntoentrega } from 'src/app/interfaces/puntoentrega';
+import { Uds } from 'src/app/interfaces/uds';
 
 @Component({
   selector: 'app-inhabilitaruds',
@@ -12,18 +14,46 @@ import { Router } from '@angular/router';
 })
 export class InhabilitarudsComponent implements OnInit {
 
-  public Inunidad: Inuds [] = [];
+  public Inunidad: Inuds[] = [];
+  puntos: Puntoentrega = {
+    idPuntoEntrega: 0,
+    NombrePE: '',
+    CodigoInternoPE: '',
+    Direccion: '',
+    Responsable: '',
+    Estado: 1,
+    Telefono: '',
+    CodigoExternoPE: '',
+    idCentrosZonales: 0,
+    BarrioPE: '',
+    Comuna: '',
+  };
+
+  unidad: Uds = {
+    idUDS: 0,
+    NombreUDS: '',
+    CodigoInternoUDS: '',
+    Direccion: '',
+    ReponsableUDS: '',
+    Estado: 1,
+    Telefono: '',
+    CodigoExternoUDS: '',
+    idPuntoEntrega: 0,
+    idCentrosZonales: 0,
+    Barrio: '',
+    Comuna: '',
+  };
 
   constructor(private activeRoute: ActivatedRoute,
-    private Service: ServicioService,private router:Router) { }
+    private Service: ServicioService, private router: Router) { }
 
-    inud:Inuds = {
-      idUDS: 0,
+  inud: Inuds = {
+    idUDS: 0,
     Estado: 0,
-    };
+  };
 
   ngOnInit() {
-
+    var estado = 0;
     const params = this.activeRoute.snapshot.params;
     console.log(params);
     if (params.id) {
@@ -31,66 +61,114 @@ export class InhabilitarudsComponent implements OnInit {
         .subscribe(res => {
           console.log(res);
           this.inud = Object(res);
+          this.unidad = Object(res);
+          estado = 1;
+          if (estado == 1) {
+            this.puntoEstado();
+          }
         }, err => {
           console.log(err);
         }
         );
 
+    }
+
   }
 
-
+  InhabilitarDatos() {
+    console.log(this.unidad.Estado);
+    if (this.puntos.Estado == 1) {
+      this.EnableDatos();
+    } else {
+      this.showMenssage3();
+      this.inud.Estado = 0;
+      this.EnableDatos2();
+    }
   }
 
-  showMenssage(){
-    Swal.fire({
-      title: 'Inhabilitado!',
-      text: 'entrega Inhabilitado',
-      type: 'success',
-      confirmButtonText: 'Ok'}).then((result) => {
-        if (result.value) {
-          
-          this.router.navigate(['/uds']);
-      
-        }
-    });
-  }
-  
-  showMenssage1(){
-    Swal.fire({
-      title: 'habilitado!',
-      text: 'entrega habilitado',
-      type: 'success',
-      confirmButtonText: 'Ok'}).then((result) => {
-        if (result.value) {
-          
-          this.router.navigate(['/uds']);
-      
-        }
-    });
+  puntoEstado() {
+    //centro zonal 
+    var id: string = this.unidad.idPuntoEntrega.toString();
+    //console.log(this.unidad.idPuntoEntrega);
+    this.Service.getPuntoid(id)
+      .subscribe(res => {
+        console.log(res);
+        this.puntos = Object(res);
+      }, err => {
+        console.log(err);
+      }
+      );
   }
 
-  EnableDatos(){
-
+  EnableDatos2() {
     this.Service.putUdsInhabilitar(this.inud.idUDS, this.inud)
       .subscribe(
         res => {
           console.log(res);
-  
-          if(this.inud.Estado == 1){
-  
-            this.showMenssage1();
-  
-          }else{
-  
-            this.showMenssage();
-          }
-  
-          
+          this.showMenssage3();
         }, err => {
           console.log(err);
-        }
-      );
+        });
   }
 
+  EnableDatos() {
+    this.Service.putUdsInhabilitar(this.inud.idUDS, this.inud)
+      .subscribe(
+        res => {
+          console.log(res);
+          if (this.inud.Estado == 1) {
+            this.showMenssage1();
+          } else {
+            this.showMenssage();
+          }
+        }, err => {
+          console.log(err);
+        });
+  }
+
+  showMenssage() {
+    Swal.fire({
+      title: 'Inhabilitado!',
+      text: 'uds Inhabilitado',
+      type: 'success',
+      confirmButtonText: 'Ok'
+    }).then((result) => {
+      if (result.value) {
+
+        this.router.navigate(['/uds']);
+
+      }
+    });
+  }
+
+  showMenssage1() {
+    Swal.fire({
+      title: 'habilitado!',
+      text: 'uds habilitado',
+      type: 'success',
+      confirmButtonText: 'Ok'
+    }).then((result) => {
+      if (result.value) {
+
+        this.router.navigate(['/uds']);
+
+      }
+    });
+  }
+
+  showMenssage3() {
+    Swal.fire({
+      title: 'Error!',
+      text: 'No es posible inhabilitar la unidad de servicio',
+      type: 'error',
+      confirmButtonText: 'Entendido'
+    }).then((result) => {
+      if (result.value) {
+
+        this.router.navigate(['/uds']);
+
+      }
+    });
+  }
 
 }
--- src/app/page/puntoentregainh/puntoentregainh.component.ts
+++ src/app/page/puntoentregainh/puntoentregainh.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicioService } from 'src/app/servicio.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Centrozonal } from 'src/app/interfaces/centrozonal';
+import { Puntoentrega } from 'src/app/interfaces/puntoentrega';
+import { Uds } from 'src/app/interfaces/uds';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-puntoentregainh',
@@ -6,10 +12,186 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./puntoentregainh.component.css']
 })
 export class PuntoentregainhComponent implements OnInit {
+  centro: Centrozonal[] = [];
+  uds: Uds[] = [];
+  punto: Puntoentrega[] = [];
+  unidades: Uds[] = [];
 
-  constructor() { }
+  centros: Centrozonal = {
+    idCentrosZonales: 0,
+    NombreCentroZonal: '',
+    idMunicipios: 1,
+    Comuna: '',
+    CodigoExternoJcz: '',
+    CodigoExternoCZ: 0,
+    Estado: 1,
+    idRegional: 2,
+  };
+
+  puntos: Puntoentrega = {
+    idPuntoEntrega: 0,
+    NombrePE: '',
+    CodigoInternoPE: '',
+    Direccion: '',
+    Responsable: '',
+    Estado: 1,
+    Telefono: '',
+    CodigoExternoPE: '',
+    idCentrosZonales: 0,
+    BarrioPE: '',
+    Comuna: '',
+  };
+
+  constructor(
+    private Service: ServicioService,
+    private router: Router,
+    private activeRoute: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    // get punto id
+    var estado = 0;
+    const params = this.activeRoute.snapshot.params;
+    //console.log(params);
+    if (params.id) {
+      this.Service.getPuntoid(params.id)
+        .subscribe(res => {
+          console.log(res);
+          this.puntos = Object(res);
+          estado = 1
+          if(estado == 1 ){
+            this.centrosEstado();
+          }
+        }, err => {
+          console.log(err);
+        }
+        );
+    }
+    // get uds
+    this.Service.getUds().subscribe(res => {
+      this.unidades = Object(res);
+    });
+  }
+
+  centrosEstado() {
+    //centro zonal 
+    var id: string = this.puntos.idCentrosZonales.toString();
+    this.Service.getCentroid(id)
+      .subscribe(res => {
+        console.log(res);
+        this.centros = Object(res);
+      }, err => {
+        console.log(err);
+      }
+      );
+  }
+
+  inhabilitarDatos() {
+    const params = this.activeRoute.snapshot.params;
+    var numero = params.id;
+    var cont2: number = 0;
+    var cont3: number = 0;
+    console.log(this.centros.Estado);
+    if (this.centros.Estado == 1) {
+      for (let numeros of this.unidades) {
+        if (numero == numeros.idPuntoEntrega && numeros.Estado == 1) {
+          var cont: number = 0;
+          var cont2 = cont + 1;
+          //console.log('Comaparado', numero, numeros.idPuntoEntrega);
+          //console.log('contador 2', cont2);
+        } else if (numero == numeros.idPuntoEntrega && numeros.Estado == 0) {
+          var cont: number = 0;
+          cont3 = cont + 1;
+          //console.log('contador 3', cont3);
+        }
+      } //cierra for()
+      if (cont2 == 0 && cont3 > 0) {
+        console.log('funciona');
+        this.updateDatos2();
+      } else {
+        //mensaje de no se puede inhabilitar
+        this.showMenssage3();
+      }
+
+    }//primer if
+    else {
+      this.showMenssage3();
+      this.puntos.Estado = 0;
+      this.updateDatos2();
+    }
+
+  }
+
+  updateDatos2() {
+    this.Service.putPunto(this.puntos.idPuntoEntrega, this.puntos)
+      .subscribe(
+        res => {
+          console.log(res);
+          if (this.puntos.Estado == 1) {
+            this.showMenssage();
+          } else if (this.puntos.Estado == 0) {
+            this.showMenssage4();
+          }
+
+        }, err => {
+          console.log(err);
+          this.showMenssage2();
+        }
+      );
+  }
+
+  showMenssage() {
+    Swal.fire({
+      title: 'Habilitado!',
+      text: 'Punto de entrega Habilitado',
+      type: 'success',
+      confirmButtonText: 'Entendido'
+    }).then((result) => {
+      if (result.value) {
+
+        this.router.navigate(['/puntoentrega']);
+
+      }
+
+    });
+  }
+  //Mensaje de error
+
+  showMenssage2() {
+    Swal.fire({
+      title: 'Error!',
+      text: 'Error al modificar el punto de entrega',
+      type: 'error',
+      confirmButtonText: 'Entendido'
+    }).then((result) => {
+      if (result.value) {
+        this.router.navigate(['/puntoentrega']);
+      }
+    });
+  }
+  showMenssage4() {
+    Swal.fire({
+      title: 'Inhabilitado!',
+      text: 'Punto de entrega inhabilitado',
+      type: 'error',
+      confirmButtonText: 'Entendido'
+    }).then((result) => {
+      if (result.value) {
+
+        this.router.navigate(['/puntoentrega']);
+
+      }
+
+    });
+  }
+
+  showMenssage3() {
+    Swal.fire({
+      title: 'Error!',
+      text: 'No es posible inhabilitar el punto de entrega',
+      type: 'error',
+      confirmButtonText: 'Entendido'
+    });
   }
 
 }
