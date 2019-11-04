62291a78bfa58bb2eb97451885fe8961db1a3920
javier alejandro castañeda celis <jcastaeda10@gmail.com>
2019-11-03 20:50:47 -0500

    auth service

--- src/app/authservice.service.ts
+++ src/app/authservice.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthserviceService {
 
-  constructor() { }
+  constructor(public http: HttpClient
+    ) { }
+    
+  api = 'http://localhost:3000';
+  headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
 }
