bd52ee5e8dea13d57960e7328e4fb08ffb0632cc
majimenezso <51141193+majimenezso@users.noreply.github.com>
2019-11-06 15:27:22 -0500

    recuentoinventario

--- src/app/page/recuentoinventario/recuentoinventario.component.ts
+++ src/app/page/recuentoinventario/recuentoinventario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Recuentoinventario } from 'src/app/interfaces/recuentoinventario';
+import { ServicioService } from '../../servicio.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recuentoinventario',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuentoinventarioComponent implements OnInit {
 
-  constructor() { }
+  recuento:Recuentoinventario[] = [];
+
+  constructor(private activeRoute: ActivatedRoute,
+    private Service: ServicioService) { }
 
   ngOnInit() {
+
+    this.Service.getrecuento()
+    .subscribe(async (data) => {
+      this.recuento = data;
+      console.log(data);
+      console.log('funciona');
+    }
+    );
+
   }
 
 }
--- src/app/page/recuentoinventarioc/recuentoinventarioc.component.ts
+++ src/app/page/recuentoinventarioc/recuentoinventarioc.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Recuentoinventario } from 'src/app/interfaces/recuentoinventario';
+import { ServicioService } from '../../servicio.service';
+import { ActivatedRoute } from '@angular/router';
+
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+import { TipoBienestarina } from '../../interfaces/tipobienestarina';
+import { Centrodistribucion } from '../../interfaces/centrodistribucion';
+import { Almacen } from '../../interfaces/almacen';
 
 @Component({
   selector: 'app-recuentoinventarioc',
@@ -7,9 +17,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuentoinventariocComponent implements OnInit {
 
-  constructor() { }
+  recuento:Recuentoinventario[] = [];
+ 
+  public tip: TipoBienestarina[] = [];
+  public cen: Centrodistribucion[] = [];
+  public alm: Almacen[] = [];
 
+  constructor(private activeRoute: ActivatedRoute,
+    private Service: ServicioService, private router:Router) { }
+
+
+    sal: Recuentoinventario = {
+
+      lote : '',
+      FechaVencimiento : 0,
+      unidadmedida1  : '',
+      cantidad: 0,
+      cantidadcontada: 0,
+      unidadmedida2  : '',
+      idTipoBienesterina: 0,
+      idAlmacenes: 0,
+      idCentroDistribucion: 0,
+    } 
   ngOnInit() {
+
+    this.Service.getTipobienestarina()
+    .subscribe(async (data) => {
+      this.tip = data;
+      console.log(data);
+      console.log('funciona');
+    }
+    );
+
+    this.Service.getcentrodistribucion()
+      .subscribe(async (data) => {
+        this.cen = data;
+        console.log(data);
+        console.log('funciona');
+      }
+      );
+
+      this.Service.getalmacen()
+      .subscribe(async (data) => {
+        this.alm = data;
+        console.log(data);
+        console.log('funciona');
+      }
+      );
+
+      
   }
+
+  onClickMe(){
+
+    
+    this.Service.postrecuento(this.sal).subscribe(res => {
+      console.log(this.sal);
+      this.showMenssage();
+      
+      },
+      err => {
+        console.log(err);
+      });
+      
+     
+     
+
+  }
+
+  showMenssage(){
+    Swal.fire({
+      title: 'Creado!',
+      text: 'Recuento Creado',
+      type: 'success',
+      confirmButtonText: 'Aceptar'
+    }).then((result) => {
+      if (result.value) {
+        
+        this.router.navigate(['/recuentoinventario']);
+    
+      }
+    });
+  }
+
 
 }
