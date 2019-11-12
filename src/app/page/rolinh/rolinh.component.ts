dd85e6c9511098ca78f65b7e1903b51c4b9738c5
javier alejandro castañeda celis <jcastaeda10@gmail.com>
2019-11-11 22:59:15 -0500

    inhabilitar rol

--- src/app/page/rolinh/rolinh.component.ts
+++ src/app/page/rolinh/rolinh.component.ts
@@ -3,6 +3,7 @@ import { ServicioService } from '../../servicio.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Rol } from 'src/app/interfaces/rol';
 import Swal from 'sweetalert2';
+import { Usuarios } from 'src/app/interfaces/usuarios';
 
 @Component({
   selector: 'app-rolinh',
@@ -18,6 +19,7 @@ export class RolinhComponent implements OnInit {
     ) { 
     
   }
+  usua: Usuarios[] = [];
   roles: Rol={
     idRolPersona: 0,
     RolPersona:'',
@@ -40,23 +42,80 @@ export class RolinhComponent implements OnInit {
           console.log(err);
         }
         );
+
+        this.Service.getUsuarios()
+        .subscribe(async (data) => {
+          this.usua = data;
+          console.log(data);
+        }
+        );
     }
 
     
 
   }
 
+  updateDatos(){
+    const params = this.activeRoute.snapshot.params;
+    var numero = params.id;
+    var cont2: number = 0;
+    var cont3: number = 0;
+    for (let numeros of this.usua) {
+      if (numero == numeros.TipoUsuario && numeros.Estado == 1) {
+        var cont: number = 0;
+        var cont2 = cont + 1;
+        //this.showMenssage3();
+      } else if(numero == numeros.TipoUsuario && numeros.Estado == 0) {
+        var cont: number = 0;
+        cont3 = cont + 1;
+      }
+    }
+    if (cont2 == 0 && cont3 >0){
+      //console.log('funciona');
+      this.updateDatos2();
+    }else{
+      //mensaje de no se puede inhabilitar
+      this.showMenssage4();
+    }
+  }
+
+  showMenssage4() {
+    Swal.fire({
+      title: 'Error!',
+      text: 'No es posible inhabilitar el rol',
+      type: 'error',
+      confirmButtonText: 'Entendido'
+    });
+  }
   // Actualizar Datos---------------------------------------------
-  updateDatos() {
+  updateDatos2() {
     this.Service.ActualizarRol(this.roles.idRolPersona, this.roles)
       .subscribe(
         res => {
           console.log(res);
-          this.showMenssage2();
+          if (this.roles.Estado == 1){
+            this.showMenssage();
+          }else if(this.roles.Estado == 0){
+            this.showMenssage5();
+          }
         }, err => {
           console.log(err);
         }
       );
+  }
+
+  showMenssage() {
+    Swal.fire({
+      title: 'Habilitado!',
+      text: 'Rol habilitado',
+      type: 'success',
+      confirmButtonText: 'Entendido'
+    }).then((res) => {
+      if(res.value){
+        console.log('confirmed');
+        this.router.navigate(['/centrozonal']);
+    }
+    });
   }
 
       //mensajes de asigancion
@@ -82,4 +141,19 @@ export class RolinhComponent implements OnInit {
       confirmButtonText: 'Entendido'
     });
   }
+  showMenssage5() {
+    Swal.fire({
+      title: 'Inhabilitado!',
+      text: 'Rol inhabilitado',
+      type: 'error',
+      confirmButtonText: 'Entendido'
+    }).then((res) => {
+      if(res.value){
+        console.log('confirmed');
+        this.router.navigate(['/centrozonal']);
+    }
+    });
+  }
+
+
 }
