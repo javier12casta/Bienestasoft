085286111597bff8b57a9b89032c4db4471ffac3
Duvan003 <duvanbv03@gmail.com>
2019-12-08 22:07:17 -0500

    Update PE

--- src/app/page/puntoentregam/puntoentregam.component.ts
+++ src/app/page/puntoentregam/puntoentregam.component.ts
@@ -16,6 +16,7 @@ export class PuntoentregamComponent implements OnInit {
 
   centros: Centrozonal[] = [];
   cz: Centrozonal[] = [];
+  pe: Puntoentrega[]=[];
   puntos: Puntoentrega = {
     NombrePE: '',
     CodigoInternoPE: '',
@@ -44,8 +45,12 @@ export class PuntoentregamComponent implements OnInit {
 
    // stop here if form is invalid
    if (this.peForm.valid) {
-     this.updateDatos();
+    this.Validar();
+    if(this.campo1 == false && this.campo2 == false && this.campo3 == false){
+      this.updateDatos();
+    }
    } else {
+    this.showMenssagenull();
    }
 
    // display form values on success
@@ -88,6 +93,13 @@ export class PuntoentregamComponent implements OnInit {
       console.log(err);
     });
 
+    this.Service.getPunto()
+      .subscribe(res => {
+        this.pe = res;
+      }, err => {
+        console.log(err);
+      });
+
     //Validador--------------------
     this.peForm = this.fb.group({
       idCentrosZonales: ['', Validators.required],
@@ -110,6 +122,60 @@ export class PuntoentregamComponent implements OnInit {
 
   get f() { return this.peForm.controls; }
 
+    //------------------Validar si exiten los datos-----------------------
+    campo1 = false;
+    campo2 = false;
+    campo3 = false;
+    Validar() {
+      console.log('nombre ingresado', this.puntos.NombrePE);
+      var contador = 0;
+      var contador2 = 0;
+      var contador3 = 0;
+      var contador4 = 0;
+      var contador5 = 0;
+      var contador6 = 0;
+      for (let d of this.pe) {
+        if (d.NombrePE == this.puntos.NombrePE) {
+          contador = contador2 + 1;
+          console.log('Nombre igual', contador);
+        }
+  
+        if (d.CodigoExternoPE == this.puntos.CodigoExternoPE) {
+          contador3 = contador4 + 1;
+  
+          console.log('Codico ExPE igual', contador3);
+        }
+  
+        if (d.CodigoInternoPE == this.puntos.CodigoInternoPE) {
+          contador5 = contador6 + 1;
+          this.campo3 = true;
+          console.log('Codico InPE igual', contador5);
+        }
+  
+      }
+      if (contador >= 1) {
+        this.campo1 = true;
+        this.showMenssage3();
+      } else {
+        this.campo1 = false;
+      }
+  
+      if (contador3 >= 1) {
+        this.campo2 = true;
+        this.showMenssage4();
+      }else{
+        this.campo2 = false;
+      }
+  
+      if (contador5 >= 1) {
+        this.campo3 = true;
+        this.showMenssage5();
+      }else{
+        this.campo3 = false;
+      }
+  
+    }
+  
   // Actualizar Datos---------------------------------------------
   updateDatos() {
     this.Service.putPunto(this.puntos.idPuntoEntrega, this.puntos)
@@ -156,5 +222,42 @@ export class PuntoentregamComponent implements OnInit {
     });
   }
 
+  showMenssagenull() {
+    Swal.fire({
+      title: 'Error',
+      text: 'Campos vacios',
+      type: 'warning',
+      confirmButtonText: 'Entendido'
+    });
+  }
 
+  showMenssage3() {
+    Swal.fire({
+      title: 'Error',
+      text: 'El nombre del punto de entrega ya exite',
+      type: 'warning',
+      confirmButtonText: 'Entendido'
+    });
+  }
+
+  showMenssage4() {
+    Swal.fire({
+      title: 'Error',
+      text: 'El código externo del punto de entre ya exite',
+      type: 'warning',
+      confirmButtonText: 'Entendido'
+    });
+  }
+
+  showMenssage5() {
+    Swal.fire({
+      title: 'Error',
+      text: 'El código interno del punto de entrega ya exite',
+      type: 'warning',
+      confirmButtonText: 'Entendido'
+    });
+  }
 }
+
+
+
